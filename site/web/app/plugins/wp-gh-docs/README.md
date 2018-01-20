03bc2b77bf8521028f8050a81442b8ccab4381f1
Austin Pray <austin@austinpray.com>
2018-01-20 17:18:40 -0600

    git subrepo clone git@github.com:austinpray/wp-gh-docs.git site/web/app/plugins/wp-gh-docs
    subrepo:
      subdir:   "site/web/app/plugins/wp-gh-docs"
      merged:   "3b097a2"
    upstream:
      origin:   "git@github.com:austinpray/wp-gh-docs.git"
      branch:   "master"
      commit:   "3b097a2"
    git-subrepo:
      version:  "0.3.1"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "a7ee886"

---
+++ site/web/app/plugins/wp-gh-docs/README.md
@@ -0,0 +1,1 @@
+# wp-gh-docs
---
+++ site/web/app/plugins/wp-gh-docs/wp-gh-docs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Plugin Name:  wp-gh-docs
+Plugin URI:   https://github.com/austinpray/wp-gh-docs
+Description:  get docs from gh
+Author:       Austin Pray
+Author URI:   https://github.com/austinpray
+License:      MIT
+*/
