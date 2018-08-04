9e8289eb49a92a4dd5ee25549a2ead731abc6d62
Austin Pray <austin@austinpray.com>
2018-08-04 10:53:44 -0500

    Delete site dir

--- site/web/app/plugins/wp-gh-docs/wp-gh-docs.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*
-Plugin Name:  wp-gh-docs
-Plugin URI:   https://github.com/austinpray/wp-gh-docs
-Description:  get docs from gh
-Author:       Austin Pray
-Author URI:   https://github.com/austinpray
-License:      MIT
-*/
